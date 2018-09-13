4a50d7878d0ce2f345254d60db3520f66555d0d1
William Bradford Larcombe <william@williambl.com>
2018-09-13 21:17:08 +0100

    Add loop going over each game and finding release date

--- my-steam-game-stats.py
+++ my-steam-game-stats.py
@@ -5,4 +5,11 @@ steamid = ""
 
 r = requests.get(f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={key}&steamid={steamid}&format=json")
 
-print(r.json())
+games = r.json()["response"]["games"]
+
+for game in games:
+    print(game)
+    if (game["playtime_forever"] > 0):
+        appid = game["appid"]
+        r = requests.get(f"https://store.steampowered.com/api/appdetails?appids={appid}&filters=release_date")
+        print(r.json())
