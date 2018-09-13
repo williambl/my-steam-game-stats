1e561530f3d85b0443059806e85abfe1c8bae4c0
William Bradford Larcombe <william@williambl.com>
2018-09-13 21:50:06 +0100

    Simplify release-date-getting loop

--- my-steam-game-stats.py
+++ my-steam-game-stats.py
@@ -8,13 +8,14 @@ steamid = environ["STEAM_ID"]
 r = requests.get(f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={key}&steamid={steamid}&format=json")
 
 games = r.json()["response"]["games"]
+games_played = [game for game in games if game["playtime_forever"] > 0]
 releaseyears = []
 
-for game in games:
-    if (game["playtime_forever"] > 0):
-        appid = game["appid"]
-        r = requests.get(f"https://store.steampowered.com/api/appdetails?appids={appid}&filters=release_date")
-        release_date = r.json()[str(appid)]["data"]["release_date"]["date"]
-        release_year = datetime.strptime(release_date, "%d %b, %Y").year
-        print(release_year)
-        releaseyears.append(release_year)
+for game in games_played:
+    appid = game["appid"]
+    r = requests.get(f"https://store.steampowered.com/api/appdetails?appids={appid}&filters=release_date")
+    release_date = r.json()[str(appid)]["data"]["release_date"]["date"]
+    release_year = datetime.strptime(release_date, "%d %b, %Y").year
+    print(release_year)
+    releaseyears.append(release_year)
+
