6e64da00a7e8ef88df4f1cead68550da4739a991
William Bradford Larcombe <william@williambl.com>
2018-09-13 21:33:48 +0100

    Add list of release years, populated by the release years of your games

--- my-steam-game-stats.py
+++ my-steam-game-stats.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 key = ""
 steamid = ""
@@ -6,10 +7,13 @@ steamid = ""
 r = requests.get(f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={key}&steamid={steamid}&format=json")
 
 games = r.json()["response"]["games"]
+releaseyears = []
 
 for game in games:
-    print(game)
     if (game["playtime_forever"] > 0):
         appid = game["appid"]
         r = requests.get(f"https://store.steampowered.com/api/appdetails?appids={appid}&filters=release_date")
-        print(r.json())
+        release_date = r.json()[str(appid)]["data"]["release_date"]["date"]
+        release_year = datetime.strptime(release_date, "%d %b, %Y").year
+        print(release_year)
+        releaseyears.append(release_year)
