17ef33eeb9fb43a6a85fb8dc44662fabbf10d769
William Bradford Larcombe <william@williambl.com>
2019-02-14 20:51:39 +0000

    Add publisher stats

--- my-steam-game-stats.py
+++ my-steam-game-stats.py
@@ -13,19 +13,27 @@ r = requests.get(f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v000
 games = r.json()["response"]["games"]
 games_played = [game for game in games if game["playtime_forever"] > 0]
 releaseyears = []
+publishers = []
 
 bar = Bar('Getting Release Dates', max=games_played.__len__())
 for game in games_played:
     appid = game["appid"]
 
-    r = requests.get(f"https://store.steampowered.com/api/appdetails?appids={appid}&filters=release_date")
+    r = requests.get(f"https://store.steampowered.com/api/appdetails?appids={appid}&filters=release_date,publishers")
 
     try:
         release_date = r.json()[str(appid)]["data"]["release_date"]["date"]
         release_year = datetime.strptime(release_date, "%d %b, %Y").year
         releaseyears.append(release_year)
     except:
-        print("error with appid " + str(appid))
+        print("error with appid " + str(appid) + " release date")
+        
+    try:
+        publisher = r.json()[str(appid)]["data"]["publishers"][0]
+        publishers.append(publisher)
+    except:
+        print("error with appid " + str(appid) + " publishers")
+
 
     bar.next()
 bar.finish()
@@ -33,6 +41,13 @@ bar.finish()
 year_frequencies = collections.Counter(releaseyears)
 print(year_frequencies)
 
+publisher_frequencies = collections.Counter(publishers)
+print(publisher_frequencies)
+
+pyplot.figure(1)
 pyplot.bar(year_frequencies.keys(), year_frequencies.values())
-pyplot.savefig("my-steam-game-stats.svg")
+pyplot.savefig("year_frequencies.svg")
+pyplot.figure(2)
+pyplot.bar(publisher_frequencies.keys(), publisher_frequencies.values())
+pyplot.savefig("publisher_frequencies.svg")
 pyplot.show()
